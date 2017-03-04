5ab9ea4cb0e001b906c16775d29067434d180d9a
Ashley <Ashley@Ashleys-MBP.station>
2017-03-05 09:54:22 +1300

    refactored

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+
 app.use(express.static(__dirname + '/public'));
 
-// viewed at h  ttp://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-
-var request = require('superagent');
-
-request
-  .get('http://www.behindthename.com/api/random.php')
-  .end(function(err, res){
-    return res
-  });
 
 app.listen(8080);
--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
-// var request = require('superagent');
-//
-// request
-//   .get('http://www.behindthename.com/api/random.php')
-//   .end(function(err, res){
-//     console.log(res)
-//   });
-//
-// console.log("HELP!")
+
+var randomName = faker.name.firstName();
 var faker = require('faker');
 var arr = []
 
-var randomName = faker.name.firstName();
-
-document.getElementById("title").innerHTML = randomName
-// console.log(faker.name.firstName())
 var joseph = function(){
   arr.push(document.getElementById("title").innerHTML)
   console.log(arr)
 }
+document.getElementById("title").innerHTML = randomName
 joseph()
